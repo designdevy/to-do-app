8abd95a6ee0d03411396db0d6ba862dc760a905a
nataliaev <nataliiaevchenko@gmail.com>
2019-11-14 12:25:34 +0100

    Add main page design

--- src/components/Mock.js
+++ src/components/Mock.js
@@ -1,19 +1,10 @@
 import React from "react";
 import LeftMenu from "./LeftMenu";
-import BottomBar from "./BottomBar";
 import MenuButton from "./MenuButton";
-import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 import Chart from "./Chart";
 import { Link } from "react-router-dom";
-import {
-  Grid,
-  Paper,
-  Typography,
-  Drawer,
-  Zoom,
-  Grow,
-  Button
-} from "@material-ui/core";
+import { Typography, Drawer, Zoom, Grow, Button } from "@material-ui/core";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { useGlobalState } from "../App";
 import { styles } from "./MockStyles";
@@ -27,52 +18,68 @@ export default withStyles(styles)(function Mock({ classes }) {
   if (matches) {
     return (
       <div className={classes.root}>
-        <header className={classes.header}>
-          <div className={classes.headerBg}></div>
-          <Zoom
-            in={matches}
-            style={{ transformOrigin: "0 0 0" }}
-            {...(matches ? { timeout: 1000 } : {})}
-          >
-            <Typography
-              variant="h2"
-              align="left"
-              className={classes.headerText}
+        <div className={classes.topBarMain}>
+          <Link to="/" className={classes.link}>
+            <Button
+              color="primary"
+              variant="outlined"
+              className={classes.button}
             >
-              Application for DUODEKA
-            </Typography>
-          </Zoom>
-        </header>
-        <Grid container>
-          <LeftMenu pressed="1" />
-          <Grid item xs={12} sm={10}>
-            <Typography variant="h2" align="right" gutterBottom>
-              Welkom to the application!
-            </Typography>
-            <Paper className={classes.paper}>
-              <Typography
-                variant="h4"
-                align="center"
-                color="primary"
-                gutterBottom
-              >
-                Cras mattis consectetur purus!
-              </Typography>
-              <Typography variant="subtitle1" gutterBottom>
-                Cras mattis consectetur purus sit amet fermentum. Cras justo
-                odio, dapibus ac facilisis in, egestas eget quam. Morbi leo
-                risus, porta ac consectetur ac, vestibulum at eros. Praesent
-                commodo cursus magna, vel scelerisque nisl consectetur et. Cras
-                mattis consectetur purus sit amet fermentum. Cras justo odio,
-                dapibus ac facilisis in, egestas eget quam. Morbi leo risus,
-                porta ac consectetur ac, vestibulum at eros. Praesent commodo
-                cursus magna, vel scelerisque nisl consectetur et. Cras mattis
-                consectetur purus sit amet fermentum.
-              </Typography>
-            </Paper>
-          </Grid>
-        </Grid>
-        <BottomBar />
+              Main page
+            </Button>
+          </Link>
+          <Link to="/mock2" className={classes.link}>
+            <Button
+              color="primary"
+              variant="outlined"
+              className={classes.button}
+            >
+              Empty Page
+            </Button>
+          </Link>
+          <Link to="/todo" className={classes.link}>
+            <Button
+              color="primary"
+              variant="outlined"
+              className={classes.button}
+            >
+              TO DO app
+            </Button>
+          </Link>
+          <Link to="/mock3" className={classes.link}>
+            <Button
+              color="primary"
+              variant="outlined"
+              className={classes.button}
+            >
+              Empty Page
+            </Button>
+          </Link>
+        </div>
+        <Zoom
+          in={matches}
+          style={{ transformOrigin: "0 0 0" }}
+          {...(matches ? { timeout: 1000 } : {})}
+        >
+          <Typography variant="h1" align="center" className={classes.mainTitle}>
+            Do your tasks together <br /> with ToDo App
+          </Typography>
+        </Zoom>
+        <Link to="/todo" className={classes.link}>
+          <Button
+            variant="contained"
+            position="center"
+            size="medium"
+            className={classes.gradientButtonDesktop}
+          >
+            Get Started
+          </Button>
+        </Link>
+        <img
+          src="https://i.etsystatic.com/isla/ce1028/31086850/isla_500x500.31086850_edi78g3d.jpg?version=0"
+          alt="violet"
+          className={classes.testImage}
+        />
       </div>
     );
   } else {
--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Typography, AppBar, Toolbar } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
 
-export default function TopMenu() {
+const styles = {
+  topBar: {
+    backgroundImage: "linear-gradient(#6675D5, #133FAC)"
+  }
+}
+
+export default withStyles(styles)(function TopMenu({ classes }) {
     return (
-    <AppBar position="static">
+    <AppBar position="static" className={classes.topBar}>
       <Toolbar>
         <Typography variant="h6">Application for DUODEKA</Typography>
       </Toolbar>
     </AppBar>
   );
-}
+})
